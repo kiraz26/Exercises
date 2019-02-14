f44f1aecfbbf17fe5c2c8c6c3fef5f0acfc714b8
kiraz26 <halilkirazz@gmail.com>
2019-02-13 21:15:18 -0500

    1

--- src/exercises1/LoginValidator2.java
+++ src/exercises1/LoginValidator2.java
@@ -6,7 +6,7 @@ public class LoginValidator2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String user = "halilkiraz";
+		String user = "johnnycash";
 		String pass = "123";
 
 		Scanner input = new Scanner(System.in);
